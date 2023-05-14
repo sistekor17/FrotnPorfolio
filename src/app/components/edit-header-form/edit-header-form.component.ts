e59e665d5c4049b243488470e7f2ffc280550710
sistekor17 <jamesserrato17@gmail.com>
2023-05-14 20:48:47 -0300

    finalizacion del edit header y crud experiencia

--- src/app/components/edit-header-form/edit-header-form.component.ts
+++ src/app/components/edit-header-form/edit-header-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Persona } from 'src/app/model/persona.model';
+import { ProfilesService } from 'src/app/services/profiles.service';
 import { UiService } from 'src/app/services/ui.service';
 
 @Component({
@@ -10,22 +11,26 @@ import { UiService } from 'src/app/services/ui.service';
 })
 export class EditHeaderFormComponent {
   @Output() onEditHeader: EventEmitter<Persona> = new EventEmitter();
-  id : number = null!;
-  nombre : string = null!;
+  persona : Persona = new Persona("","","","","","","");
+  
+  nombre : string = this.persona.nombre;
   titulo : string = null!;
   sobreMi : string = null!;
   banner : string = null!;
-  img : string = null!;
+  img : string = null!  
   email : string = null!;
   curriculum : string = null!;
   showEditHeader : boolean = false;
   subscription? : Subscription;
   
   //constructor
-  constructor(private uiServ : UiService){
+  constructor(private uiServ : UiService, private ProfileServ : ProfilesService){
     this.subscription = this.uiServ.onToggleEditHeader().subscribe(
       (value) => {this.showEditHeader = value}
     );
+  }
+  ngOnInit() {
+    this.ProfileServ.getPersona().subscribe(data => {this.persona = data});
   }
 
   ontShowEditHeader(){
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component} from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Persona } from 'src/app/model/persona.model';
 import { ProfilesService } from 'src/app/services/profiles.service';
+import { TokenService } from 'src/app/services/token.service';
 import { UiService } from 'src/app/services/ui.service';
 
 
@@ -15,9 +16,9 @@ export class HeaderComponent {
   persona : Persona = new Persona("","","","","","","");
   showEditHeader : boolean = false;
   subscription? : Subscription;
-  idEdit : number = null!;
+  isLogged : boolean = false; 
 
-  constructor(public profilesService : ProfilesService, private uiServ : UiService){
+  constructor(public profilesService : ProfilesService, private uiServ : UiService, private tokenService : TokenService){
     this.subscription = this.uiServ.onToggleEditHeader().subscribe(
       (value) => {this.showEditHeader = value}
     );
@@ -25,6 +26,11 @@ export class HeaderComponent {
 
   ngOnInit(): void{
     this.profilesService.getPersona().subscribe(data =>{this.persona = data});
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    }else{
+      this.isLogged = false;
+    }
   }
   ontShowEditHeader(){
     this.uiServ.toggleVisibilityEditHeader();
